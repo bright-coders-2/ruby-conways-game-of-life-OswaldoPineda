650fe9b722249b08c6c32cc73068f1d08481ba95
OswaldoPineda <carlos-1502010@hotmail.com>
2019-04-04 11:55:34 -0600

    The class game functionality was finished.
    This class is responsible of create the class world when its called,
    of print the board in console and evaluate the rules of the game
    
    	modified: lib/conway/GOL/game.rb

--- lib/conway/GOL/cell.rb
+++ lib/conway/GOL/cell.rb
@@ -2,28 +2,28 @@ module Conway
     class Cell
         attr_accessor :alive, :mark, :x, :y
         def initialize(x,y)
-            @alive = false
-            @mark = "-".black
-            @x = x
-            @y = y
+          @alive = false
+          @mark = "-".black
+          @x = x
+          @y = y
         end
     
         def alive?
-            @alive
+          @alive
         end
     
         def dead?
-            !@alive
+          !@alive
         end
     
         def die!
-            @mark = "-".black
-            @alive = false
+        @mark = "-".black
+          @alive = false
         end
     
         def live!
-            @mark = "@".green
-            @alive = true
+          @mark = "@".green
+          @alive = true
         end
     
     end
