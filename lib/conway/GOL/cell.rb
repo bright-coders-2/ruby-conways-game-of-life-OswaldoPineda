1492d8ddbc8e0a0a977081afb39dc0fa505ac404
OswaldoPineda <carlos-1502010@hotmail.com>
2019-04-04 10:38:03 -0600

    Finish the class cell.
    The class cell was created with its methods and attributes
    what they return and change the status of the cell.
    
    	modified: lib/conway/GOL/cell.rb

--- lib/conway/GOL/cell.rb
+++ lib/conway/GOL/cell.rb
@@ -1,26 +1,30 @@
-class Cell
-    attr_accessor :alive, :value, :x, :y
-    def initialize()
-        @alive = false
-        @value = "-"
+module Conway
+    class Cell
+        attr_accessor :alive, :mark, :x, :y
+        def initialize(x,y)
+            @alive = false
+            @mark = "-".black
+            @x = x
+            @y = y
+        end
+    
+        def alive?
+            @alive
+        end
+    
+        def dead?
+            !@alive
+        end
+    
+        def die!
+            @mark = "-".black
+            @alive = false
+        end
+    
+        def live!
+            @mark = "@".green
+            @alive = true
+        end
+    
     end
-
-    def alive?
-        @alive
-    end
-
-    def dead?
-        !@alive
-    end
-
-    def die!
-        @value = "-"
-        @alive = false
-    end
-
-    def live!
-        @value = "@"
-        @alive = true
-    end
-
 end
