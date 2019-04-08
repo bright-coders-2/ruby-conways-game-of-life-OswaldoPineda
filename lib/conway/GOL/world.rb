93884b2e5358a546942b88c861c916cbc18d6897
OswaldoPineda <carlos-1502010@hotmail.com>
2019-04-08 17:04:08 -0500

    Fix identation with rubocop
    The variables x and y has been changed for pos_x and pos_y in the files cell.rb and world.rb.

--- lib/conway/GOL/world.rb
+++ lib/conway/GOL/world.rb
@@ -1,70 +1,68 @@
-
 module Conway
-    class World
-        attr_accessor :rows, :cols, :cells, :cell_grid
-        def initialize(rows=30, cols=110,user_cell_position=[])
-            @rows = rows
-            @cols = cols
-            @cells = []
-            @cell_grid =  Array.new(rows) do |row|
-                            Array.new(cols) do |col|
-                                cell = Conway::Cell.new(col, row)
-                                cells << cell
-                                cell
-                            end
-                        end
-            @user_cell_position = user_cell_position
-            create_cell(@user_cell_position)
+  class World
+    attr_accessor :rows, :cols, :cells, :cell_grid
+    def initialize(rows = 30, cols = 110, user_cell_position = [])
+      @rows = rows
+      @cols = cols
+      @cells = []
+      @cell_grid = Array.new(rows) do |row|
+        Array.new(cols) do |col|
+          cell = Conway::Cell.new(col, row)
+          cells << cell
+          cell
         end
-    
-        def create_cell(user_cell_position)
-          user_cell_position.each do |seed| 
-            cell_grid[seed[0]][seed[1]].alive = true
-          end
-        end
-        
-        def neighbours_around_cell(cell)
-            live_neighbours = 0
-            if cell.y > 0
-                candidate = cell_grid[cell.y - 1][cell.x] 
-                candidate.alive? ? live_neighbours +=1 : '' 
-            end
-            if cell.y > 0 && cell.x < (cols - 1)
-                candidate = cell_grid[cell.y - 1][cell.x + 1]
-                candidate.alive? ? live_neighbours +=1 : '' 
-            end
-            if cell.x < (cols - 1)
-                candidate = cell_grid[cell.y][cell.x + 1]
-                candidate.alive? ? live_neighbours +=1 : '' 
-            end
-            if cell.y < (rows - 1) && cell.x < (cols - 1)
-                candidate = cell_grid[cell.y + 1][cell.x + 1]
-                candidate.alive? ? live_neighbours +=1 : '' 
-            end
-            if cell.y < (rows - 1)
-                candidate = cell_grid[cell.y + 1][cell.x]
-                candidate.alive? ? live_neighbours +=1 : '' 
-            end
-            if cell.y < (rows - 1) && cell.x > 0 
-                candidate = cell_grid[cell.y + 1][cell.x - 1]
-                candidate.alive? ? live_neighbours +=1 : '' 
-            end
-            if cell.x > 0
-                candidate = cell_grid[cell.y][cell.x - 1]
-                candidate.alive? ? live_neighbours +=1 : '' 
-            end
-            if cell.y > 0 && cell.x > 0
-                candidate = cell_grid[cell.y - 1][cell.x - 1]
-                candidate.alive? ? live_neighbours +=1 : '' 
-            end
-            live_neighbours
-        end
-
-        def random_generator_cell
-          cells.each do |cell|
-            cell.alive = [true, false].sample
-          end
-        end
-    
+      end
+      @user_cell_position = user_cell_position
+      create_cell(@user_cell_position)
     end
+
+    def create_cell(user_cell_position)
+      user_cell_position.each do |seed|
+        cell_grid[seed[0]][seed[1]].alive = true
+      end
+    end
+    
+    def neighbours_around_cell(cell)
+      live_neighbours = 0
+      if cell.pos_y > 0
+        candidate = cell_grid[cell.pos_y - 1][cell.pos_x]
+        candidate.alive? ? live_neighbours += 1 : ''
+      end
+      if cell.pos_y > 0 && cell.pos_x < (cols - 1)
+        candidate = cell_grid[cell.pos_y - 1][cell.pos_x + 1]
+        candidate.alive? ? live_neighbours += 1 : ''
+      end
+      if cell.pos_x < (cols - 1)
+        candidate = cell_grid[cell.pos_y][cell.pos_x + 1]
+        candidate.alive? ? live_neighbours += 1 : ''
+      end
+      if cell.pos_y < (rows - 1) && cell.pos_x < (cols - 1)
+        candidate = cell_grid[cell.pos_y + 1][cell.pos_x + 1]
+        candidate.alive? ? live_neighbours += 1 : ''
+      end
+      if cell.pos_y < (rows - 1)
+        candidate = cell_grid[cell.pos_y + 1][cell.pos_x]
+        candidate.alive? ? live_neighbours += 1 : ''
+      end
+      if cell.pos_y < (rows - 1) && cell.pos_x > 0
+        candidate = cell_grid[cell.pos_y + 1][cell.pos_x - 1]
+        candidate.alive? ? live_neighbours += 1 : ''
+      end
+      if cell.pos_x > 0
+        candidate = cell_grid[cell.pos_y][cell.pos_x - 1]
+        candidate.alive? ? live_neighbours += 1 : ''
+      end
+      if cell.pos_y > 0 && cell.pos_x > 0
+        candidate = cell_grid[cell.pos_y - 1][cell.pos_x - 1]
+        candidate.alive? ? live_neighbours += 1 : ''
+      end
+      live_neighbours
+    end
+
+    def random_generator_cell
+      cells.each do |cell|
+        cell.alive = [true, false].sample
+      end
+    end
+  end
 end
--- spec/conway/GOL/world_spec.rb
+++ spec/conway/GOL/world_spec.rb
@@ -1,52 +1,57 @@
-require "../../spec_helper"
+require '../../spec_helper'
 
 RSpec.describe Conway::Game do
-
   describe '#create_cell' do
     it 'gives life to a cell' do
-      world = Conway::World.new(30,100,[[1,1]])
+      world = Conway::World.new(30, 100, [[1, 1]])
       expect(world.cell_grid[1][1].alive).to eq(true)
     end
   end
 
   describe '#neighbours_around_cell' do
     it 'cell with 1 live neighbour' do
-      world = Conway::World.new(30,100,[[1,1],[2,1]])
-      expect(world.neighbours_around_cell(world.cell_grid[0][1])).to eq(1) 
+      world = Conway::World.new(30, 100, [[1, 1], [2, 1]])
+      expect(world.neighbours_around_cell(world.cell_grid[0][1])).to eq(1)
     end
     
     it 'cell with 2 live neighbour' do
-      world = Conway::World.new(30,100,[[1,1],[2,1],[1,2]])
-      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(2) 
+      world = Conway::World.new(30, 100, [[1, 1], [2, 1], [1, 2]])
+      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(2)
     end
 
     it 'cell with 3 live neighbour' do
-      world = Conway::World.new(30,100,[[1,1],[2,1],[1,2],[2,2]])
-      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(3) 
+      positions = [[1, 1], [2, 1], [1, 2], [2, 2]]
+      world = Conway::World.new(30, 100, positions)
+      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(3)
     end
 
     it 'cell with 4 live neighbour' do
-      world = Conway::World.new(30,100,[[1,1],[2,1],[1,2],[2,2],[0,2]])
-      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(4) 
+      positions = [[1, 1], [2, 1], [1, 2], [2, 2], [0, 2]]
+      world = Conway::World.new(30, 100, positions)
+      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(4)
     end
 
     it 'cell with 5 live neighbour' do
-      world = Conway::World.new(30,100,[[1,1],[2,1],[1,2],[2,2],[0,2],[0,1]])
-      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(5) 
+      positions = [[1, 1], [2, 1], [1, 2], [2, 2], [0, 2], [0, 1]]
+      world = Conway::World.new(30, 100, positions)
+      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(5)
     end
 
     it 'cell with 6 live neighbour' do
-      world = Conway::World.new(30,100,[[1,1],[2,1],[1,2],[2,2],[0,2],[0,1],[1,0]])
-      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(6) 
+      positions = [[1, 1], [2, 1], [1, 2], [2, 2], [0, 2], [0, 1], [1, 0]]
+      world = Conway::World.new(30, 100, positions)
+      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(6)
     end
 
     it 'cell with 7 live neighbour' do
-      world = Conway::World.new(30,100,[[1,1],[2,1],[1,2],[2,2],[0,2],[0,1],[1,0],[2,0]])
-      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(7) 
+      positions = [[1, 1], [2, 1], [1, 2], [2, 2], [0, 2], [0, 1], [1, 0], [2, 0]]
+      world = Conway::World.new(30, 100, positions)
+      expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(7)
     end
 
     it 'cell with 8 live neighbour' do
-      world = Conway::World.new(30,100,[[1,1],[2,1],[1,2],[2,2],[0,2],[0,1],[1,0],[2,0],[0,0]])
+      positions = [[1, 1], [2, 1], [1, 2], [2, 2], [0, 2], [0, 1], [1, 0], [2, 0], [0, 0]]
+      world = Conway::World.new(30, 100, positions)
       expect(world.neighbours_around_cell(world.cell_grid[1][1])).to eq(8) 
     end
   end
